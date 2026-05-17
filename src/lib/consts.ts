5e603faadc034006bc28dcecfc8fa4554caff8aa
Radim Staněk <gtomyasek@gmail.com>
2026-05-17 13:11:25 +0200

    fix: refactor consts to initialize services with config objects

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -5,5 +5,9 @@ export const AUTH_URL = 'https://id.authapex.net';
 export const APP_NAME = 'authapex';
 export const REDIRECT_URL = ''; // Not needed
 
-export const AUTHORIZATION_SERVICE = new AuthorizationService(AUTH_URL, APP_NAME, REDIRECT_URL);
-export const PERMISSION_SERVICE = new PermissionService(APP_NAME);
+export const AUTHORIZATION_SERVICE = new AuthorizationService({
+  authApi: AUTH_URL,
+  app: APP_NAME,
+  redirectUrl: REDIRECT_URL,
+});
+export const PERMISSION_SERVICE = new PermissionService({ app: APP_NAME });
